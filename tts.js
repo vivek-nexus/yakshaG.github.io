b87874ba32bb19b3381645e33cf3535d455bc81b
Vivek <audit536@gmail.com>
2020-07-07 22:25:28 +0530

    Add files via upload

--- tts.js
+++ tts.js
@@ -8,17 +8,68 @@ const speakButton=document.querySelector('#speak-button');
 const pauseButton=document.querySelector('#pause-button');
 const resumeButton=document.querySelector('#resume-button');
 const stopButton=document.querySelector('#stop-button');
+const prevButton=document.querySelector('#prev-button');
+const nextButton=document.querySelector('#next-button');
 const readingText=document.querySelector('#reading-text');
 
+//Setting Variables
 let voices=[];
 let date= new Date();
 let stopAll=false;
 let pause=false;
+let prev=false;
+let next=false;
 pauseButton.disabled=true;
 resumeButton.disabled=true;
+prevButton.disabled=true;
+nextButton.disabled=true;
 
 const synthObj=window.speechSynthesis;
 
+//Execution Statements and Event Handlers
+populateVoices();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+fullScreenButton.addEventListener('click', function(){
+  document.documentElement.webkitRequestFullScreen();
+});
+
+resetButton.addEventListener('click', function(){
+  inputText.value="";
+  inputText.focus();
+});
+
+speakButton.addEventListener('click', function(){
+  speakButton.disabled=true;
+  parseSentences();
+});
+stopButton.addEventListener('click', stopAllFunction);
+
+pauseButton.addEventListener('click',pauseFunction);
+
+prevButton.addEventListener('click',function(){
+  speechSynthesis.cancel();
+  prev=true;
+});
+
+nextButton.addEventListener('click',function(){
+  speechSynthesis.cancel();
+  next=true;
+});
+
+
+
+
+
+
+
+
+
+//FUNCTIONS SECTION
+
+//Fetches and Populates the Voices Array in Alphabetical Order
 function populateVoices(){
   voices=synthObj.getVoices().sort(function(a,b){
     if(a.name.toUpperCase() < b.name.toUpperCase())
@@ -39,26 +90,69 @@ function populateVoices(){
   }
 }
 
-function stopAllFunction(){
-  speechSynthesis.cancel(); 
-  readingText.textContent=''; 
-  stopAll=true;
+
+//Parses text content and breaks into sentences
+//Loops over sentences and sends to showReadingText
+//Handles Previous, Stop, Pause, Resume and Next Buttons
+async function parseSentences(){
+  const selectedVoice = inputVoice.selectedOptions[0].getAttribute('data-voice-name');
+
+  if(selectedVoice===''){
+    alert("Please select Voice from the list");
+    speakButton.disabled=false;
+    return;
+  }
+
+  pauseButton.disabled=false;
+  resumeButton.disabled=false;
+  prevButton.disabled=false;
+  nextButton.disabled=false;
+
+  let sentences=inputText.value.split(/[.|!|?]+/g);
+
+  console.log("Finished parsing sentences!")
+  for(let i=0; i<sentences.length; i++){
+    
+    if(pause===true){
+      await pausedResume();
+      i--;
+      pause=false;
+    }
+
+    if(prev===true){
+      i=i-2;
+      prev=false;
+    }
+
+    if(stopAll===true){
+      stopAll=false;
+      break;
+    }
+    console.log(`Sentence ${i+1} sent for reading...`)
+    await showReadingText(sentences[i]);
+  }
+
+  speakButton.disabled=false;
+  pauseButton.disabled=true;
+  resumeButton.disabled=true;
+  prevButton.disabled=true;
+  nextButton.disabled=true;
+
 }
 
-function pauseFunction(){
-  speechSynthesis.cancel(); 
-  pause=true;
+//Displays Reading text and calls speaker
+//Waits until speaking is over
+//Returns promise after completion
+async function showReadingText(textPart){
+  // console.log(textPart);
+  readingText.textContent=textPart;
+  readingText.scrollIntoView();
+  await speaker(textPart);
+  return new Promise(resolve => {resolve();});
 }
 
-
-async function pausedResume(){
-  return new Promise(resolve => {
-    resumeButton.onclick = resolve; 
-    stopButton.onclick = resolve;
-  });
-}
-
-
+//TTS Speaking function
+//Returns promise after completing or cancelling TTS speak
 function speaker(textPart){
   const speakObj = new SpeechSynthesisUtterance(textPart);
   const selectedVoice = inputVoice.selectedOptions[0].getAttribute('data-voice-name');
@@ -74,84 +168,66 @@ function speaker(textPart){
   return new Promise(resolve => {speakObj.onend = resolve;});
 }
 
-async function showReadingText(textPart){
-  // console.log(textPart);
-  readingText.textContent=textPart;
-  readingText.scrollIntoView();
-  await speaker(textPart);
-  return new Promise(resolve => {resolve();});
+//Called by Pause Button
+//Stops TTS and sets variable values
+function pauseFunction(){
+  speechSynthesis.cancel(); 
+  pause=true;
 }
 
-async function parseSentences(){
-  const selectedVoice = inputVoice.selectedOptions[0].getAttribute('data-voice-name');
+//Called by Stop Button
+//Stops TTS and sets variable values
+function stopAllFunction(){
+  speechSynthesis.cancel(); 
+  readingText.textContent=''; 
+  stopAll=true;
+}
 
-  if(selectedVoice===''){
-    alert("Please select Voice from the list");
-    speakButton.disabled=false;
-    return;
-  }
-
-  pauseButton.disabled=false;
-  resumeButton.disabled=false;
-
-  let sentences=inputText.value.split(/[.|!|?]+/g);
-
-  console.log("Finished parsing sentences!")
-  for(let i=0; i<sentences.length; i++){
-    
-    if(pause===true){
-      await pausedResume();
-      i--;
-      pause=false;
-    }
-
-    if(stopAll===true){
-      stopAll=false;
-      break;
-    }
-    console.log(`Sentence ${i+1} sent for reading...`)
-    await showReadingText(sentences[i]);
-  }
-
-  speakButton.disabled=false;
-  pauseButton.disabled=true;
-  resumeButton.disabled=true;
-
+//Called by Resume Button
+//Resolves promises for pause wait
+async function pausedResume(){
+  return new Promise(resolve => {
+    resumeButton.onclick = resolve; 
+    stopButton.onclick = resolve;
+    prevButton.onclick = resolve;
+    nextButton.onclick = resolve;
+  });
 }
 
 
-populateVoices();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoices;
-}
-
-// console.log(voices);
-
-// if(voices.length==0)
-// alert("Sorry! This browser does not have support for Text to Speech. Please try Chrome or Firefox.");
 
 
-// speakButton.addEventListener('click', speaker);
-fullScreenButton.addEventListener('click', function(){
-  document.documentElement.webkitRequestFullScreen();
+
+
+
+
+//ADD TO HOME SCREEN PROGRESSIVE WEBAPP
+let deferredPrompt;
+const addBtn = document.querySelector('#add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
 });
-
-
-resetButton.addEventListener('click', function(){
-  inputText.value="";
-  inputText.focus();
-});
-
-speakButton.addEventListener('click', function(){
-  speakButton.disabled=true;
-  parseSentences();
-});
-stopButton.addEventListener('click', stopAllFunction);
-pauseButton.addEventListener('click',pauseFunction);
-
-
-
-
-
-
 
