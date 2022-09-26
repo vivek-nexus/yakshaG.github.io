51577d15ca3dd55d7f9dae57950a5ae34ec0fa22
Vivek <audit536@gmail.com>
2022-09-26 23:08:44 +0530

    Remove unwanted imports

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import CharacterCard from "../components/CharacterCard"
 import Footer from "../components/Footer"
 import NavBar from "../components/NavBar"
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
