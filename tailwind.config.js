4d68aca459d0b56933afaf473cd95dd30103d934
Vivek G <audit536@gmail.com>
2023-12-02 17:11:00 +0530

    Update resume and tweak design project page

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       boxShadow: {
         "designProjectCard": ["rgba(0, 0, 0, 0.16) 0px 3px 6px", "0px 3px 6px rgba(0, 0, 0, 0.23)"],
-        "reverseShadow": ["rgba(0, 0, 0, 0.16) 0px -4px 4px 2px"],
+        "reverseShadow": ["rgba(0, 0, 0, 0.25) 0px -4px 16px 4px"],
         "innerShadow": ["inset 0px 0px 16px -2px rgba(0, 0, 0, 0.25)", "rgba(0, 0, 0, 0.16) 0px 3px 6px"]
       }
     },
