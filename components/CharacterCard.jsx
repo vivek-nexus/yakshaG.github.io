df0e087932ec745268bcc4ed53246442c1270abf
yakshaG <audit536@gmail.com>
2024-08-22 09:56:37 +0530

    Fix icon sizes

--- components/CharacterCard.jsx
+++ components/CharacterCard.jsx
@@ -29,7 +29,7 @@ function CardContent(props) {
                     } */}
 
                     <h3>
-                        <span className={`text-secondary-300 material-symbols-outlined ${props.heroCard ? `md:text-5xl text-3xl` : `text-3xl`} pb-4`}>
+                        <span className={`text-secondary-300 material-symbols-outlined ${props.heroCard ? `md:!text-5xl !text-3xl` : `!text-3xl`} pb-4`}>
                             {characterCardData[props.cardNumber - 1].cardIcon}
                         </span>
                     </h3>
