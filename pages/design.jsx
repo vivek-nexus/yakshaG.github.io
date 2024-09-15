e5e45e92bc3c8a81a346fa4a45828750b17b048e
yakshaG <audit536@gmail.com>
2024-09-15 18:53:37 +0530

    Add challenge videos

--- pages/design.jsx
+++ pages/design.jsx
@@ -53,6 +53,17 @@ function Design() {
                         </div>
                     </div>
 
+                    <div className="mx-auto mb-6 p-2 bg-primary-700 w-2 rounded"></div>
+
+                    <div className="mb-24">
+                        <h2 className="text-4xl text-center text-primary-700 mb-6">UI Design + Development Challenges</h2>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                            <iframe width="100%" className="aspect-video rounded-lg shadow-designProjectCard" src="https://www.youtube.com/embed/JIHhMmOO0Vs?si=iIVn68urJpf0_zSk" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                            <iframe width="100%" className="aspect-video rounded-lg shadow-designProjectCard" src="https://www.youtube.com/embed/5iUes7wM_fE?si=bc7LemYnylvCofP5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                        </div>
+
+                    </div>
+
                     <div className="mb-24">
                         <div className="flex justify-between items-center">
                             <Link href="/"><a><Button type="secondary"><span className="material-icons-round mr-1">home</span></Button></a></Link>
