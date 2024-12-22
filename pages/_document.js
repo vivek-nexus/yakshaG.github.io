7447855a84b6058b4701303d203c77f6ca1b3e25
yakshaG <audit536@gmail.com>
2024-12-22 18:05:28 +0530

    Fix font name

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=Suse:wght@300;400;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=SUSE:wght@300;400;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
